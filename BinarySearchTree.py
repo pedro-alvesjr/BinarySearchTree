e209e26154daca959b33600ac20a1c6bcd4919f8
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-25 23:31:28 -0300

    Update BinarySearchTree.py
    Added insert(value) method.

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -8,3 +8,24 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
+    def insert(self, value):
+            new_node = Node(value)
+            if self.root == None:
+                self.root = new_node
+                return True
+            temp = self.root
+            while True:
+                if new_node.value == temp.value:
+                    return False
+                if new_node.value < temp.value:
+                    if temp.left == None:
+                        temp.left = new_node
+                        return True
+                    temp = temp.left
+                else:
+                    if temp.right == None:
+                        temp.right = new_node
+                        return True
+                    temp = temp.right
+
